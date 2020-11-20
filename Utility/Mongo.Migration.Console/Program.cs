4f543321247177f5f34e33ff02ae15e4053da447
Vic <v.g.albonian@gmail.com>
2020-11-20 12:04:42 +0800

    Added mongo migration

---
+++ Utility/Mongo.Migration.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using MongoDBMigrations;
+using System;
+using System.Reflection;
+using Vc.DAL.Mongo.Repositories;
+
+namespace Mongo.Migration.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables();
+
+            var config = builder.Build();
+
+            Console.WriteLine("Migrating!");
+
+            Assembly assemblyWithMigrations = typeof(AbstractRepository).Assembly;
+
+            string connectionString = config.GetConnectionString("mongodb");
+            string databaseName = config.GetValue<string>("database");
+            new MigrationEngine().UseDatabase(connectionString, databaseName)
+                .UseAssembly(assemblyWithMigrations) //Required//Required to use specific db.UseAssembly(assemblyWithMigrations) //Required
+                .UseSchemeValidation(false) //Optional true or false
+                .Run(); // Execution call. Might be called without targetVersion, in that case, the engine will choose the latest available version.
+
+        }
+    }
+}
---
+++ Vc.DAL.Mongo/Migrations/InitialMigration.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using MongoDBMigrations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vc.DAL.Mongo.Migrations
+{
+    public class InitialMigration : IMigration
+    {
+        public MongoDBMigrations.Version Version => new MongoDBMigrations.Version(0,1,0);
+
+        public string Name => "Initial Migration";
+
+        public void Up(IMongoDatabase database)
+        {
+            database.CreateCollection("room");
+        }
+
+        public void Down(IMongoDatabase database)
+        {
+            database.DropCollection("room");
+        }
+
+        
+    }
+}
